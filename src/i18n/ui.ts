66bf83c8ed904a7bd6fdeeeeea600ba9788dd7f5
Andres Soler <nombiemugi@gmail.com>
2026-07-14 13:10:35 -0500

    finally solving cvs bug

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -40,7 +40,7 @@ export const ui = {
     "hero.badge.designer": "Designer",
     "hero.badge.polyglot": "Polyglot",
     "hero.resume": "Resume",
-    "hero.cvFile": "/HvTmpEnglish.pdf",
+    "hero.cvFile": "/resume-english.pdf",
 
     // Introduction cards
     "intro.fullstack.title": "Full Stack",
@@ -140,7 +140,7 @@ export const ui = {
     "hero.badge.designer": "Diseñador",
     "hero.badge.polyglot": "Políglota",
     "hero.resume": "Hoja de vida",
-    "hero.cvFile": "/HvTmpSpanish.pdf",
+    "hero.cvFile": "/resume-spanish.pdf",
 
     // Introduction cards
     "intro.fullstack.title": "Full Stack",
